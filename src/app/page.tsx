cc15ed7022d3438962ab6fd42d2e3afe858e59bf
duyanh020405 <147359622+duyanh020405@users.noreply.github.com>
2024-08-25 21:25:59 +0700

    ss9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,145 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { Input} from "@/components/ui/input"
+import { InputFile } from '@/components/Bai2'
+import { Slider } from "@/components/ui/slider"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import { Label } from '@/components/ui/label'
 
-export default function Home() {
+
+export default function page() {
+  const [value,setValue]=useState(33)
+  const change =(newValue:number)=>{
+    setValue(newValue)
+
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <h1>Bai 1</h1>
+      <Button style={{backgroundColor:'red', color:'white'}} variant="outline">Detructive</Button>
+      <Button style={{backgroundColor:'bule', color:'white'}} variant="outline">Privemary</Button>
+      <Button variant="outline">Secondary</Button>
+      <Button style={{backgroundColor:'blue', color:'white'}} variant="outline">Size Langer</Button>
+      <Button style={{backgroundColor:'blue', color:'white' ,position:'relative',height:30}} variant="outline">Size Small</Button>
+      <h1>Bai 2</h1>
+      <Input></Input>
+      <InputFile></InputFile>
+      <h1>Bai 3</h1>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+                   <BreadcrumbLink href="/">Home</BreadcrumbLink>
+           </BreadcrumbItem>
+           <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                   <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+              </BreadcrumbItem>
+           <BreadcrumbSeparator />
+           <BreadcrumbItem>
+                       <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+            </Breadcrumb>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Breadcrumb>
+  <BreadcrumbList>
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/">Home</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/components">List</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+  </BreadcrumbList>
+</Breadcrumb>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <h1>Bai 4</h1>
+      <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Mon hoc" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Html</SelectItem>
+    <SelectItem value="dark">Css</SelectItem>
+    <SelectItem value="system">Java</SelectItem>
+    <SelectItem value="dark">Next JS</SelectItem>
+    <SelectItem value="system">CreatJs</SelectItem>
+  </SelectContent>
+  <h1>Bai 5</h1>
+  <InputOTP maxLength={6}>
+  <InputOTPGroup>
+    <InputOTPSlot index={0} />
+    <InputOTPSlot index={1} />
+    <InputOTPSlot index={2} />
+  </InputOTPGroup>
+  <InputOTPSeparator />
+  <InputOTPGroup>
+    <InputOTPSlot index={3} />
+    <InputOTPSlot index={4} />
+    <InputOTPSlot index={5} />
+  </InputOTPGroup>
+</InputOTP>
+</Select>
+<h1>Bai7</h1>
+<Slider defaultValue={[33]} value={[value]} onValueChange={(newValue) => setValue(newValue[0])} max={100} step={1} />
+  <p>Slider : {value}</p>
+  <h2>Bai 8</h2>
+  <div style={{border :' 3px solid black' , width:300, height:400 ,}}>
+    <h1 className='text-3xl'>Creat a account</h1>
+    <p>Enter you email below to creat your acount</p>
+    <div style={{display:'flex' , flexDirection:'row' , gap:30}}>
+    <Button>Github <img style={{position:'relative', width:30}} src="https://pngimg.com/uploads/github/github_PNG40.png" alt="" /></Button>
+    <Button>Google <img style={{position:'relative', width:30}} src="https://th.bing.com/th/id/R.7e557f1c0864829c54c300d15bee69f4?rik=fjZN1AYH30vXIw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fgoogle%2fgoogle_PNG19635.png&ehk=ZmsumEtoeJQhKoUzQTZO2TEbYPBu0%2b7EFdjmJ3qljls%3d&risl=&pid=ImgRaw&r=0" alt="" /></Button>
+    </div>
+    <br />
+    <br />
+    <div style={{display:'flex' , flexDirection:'row' , gap:10}}><div style={{backgroundColor:'black' , width:120 , height:1}}></div>Or<div style={{backgroundColor:'black' , width:140 , height:1}}></div></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    <br />
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="abc@gmail.com" />
+    </div>
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">PassWord</Label>
+      <Input type="email" id="email" placeholder="Email" />
+      <Button style={{backgroundColor:'black' , color:'white'}}> Submit</Button>
+    </div>
+    <br /><br />
+    
+  </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+
+
+
+
+
+
+      
+
+    </div>
+  )
 }
